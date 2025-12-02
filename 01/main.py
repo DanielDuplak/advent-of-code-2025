85c75250ab40cff9b08406ae6adb81ff45994ae8
Daniel Duplák <daniel.duplak@student.tuke.sk>
2025-12-02 21:56:33 +0100

    first commit - 01 and 02

---
+++ 01/main.py
@@ -0,0 +1,38 @@
+
+
+with open("01/input.txt", "r") as file:
+    lines = file.readlines()
+    lines = [code.strip() for code in lines]
+    codes = [(code[0:1], int(code[1:])) for code in lines]
+
+def part1(codes):
+    curr = 50
+    count = 0
+    for code in codes:
+        direction, value = code
+        if direction == "R":
+            curr = (curr + value) % 100
+        elif direction == "L":
+            curr = (curr - value) % 100
+        if curr == 0:
+            count += 1
+    return count
+
+print("Part 1:", part1(codes))
+print(50//100)
+
+def part2(codes):
+    curr = 50
+    count = 0
+    for code in codes:
+        for _ in range(code[1]):
+            if code[0] == "R":
+                curr = (curr + 1) % 100
+            else: 
+                curr = (curr - 1) % 100
+            if curr == 0:
+                count += 1
+        
+    return count
+
+print("Part 2:", part2(codes))
---
+++ 02/main.py
@@ -0,0 +1,61 @@
+
+with open('02/input.txt') as f:
+    line = f.readline().strip()
+    id_ranges = line.split(',')
+
+
+#print(id_ranges)
+
+def part1(id_ranges):
+    invalid_ids = set()
+    for id_range in id_ranges:
+        start, end = map(int, id_range.split('-'))
+        for curr_id in range(start, end + 1):
+            id_len = len(str(curr_id))
+            if id_len % 2 != 0:
+                continue
+            if str(curr_id)[:id_len // 2] == str(curr_id)[id_len // 2:]:
+                invalid_ids.add(curr_id)
+    return sum(invalid_ids)
+
+#print("Part 1:", part1(id_ranges))
+
+def is_duplicate(string, sub_string):
+    string_length = len(string)
+    sub_string_length = len(sub_string)
+    if string_length == sub_string_length:
+        return False
+    if string_length % sub_string_length != 0:
+        return False
+    for i in range(0, string_length, sub_string_length):
+        if string[i:i+sub_string_length] != sub_string:
+            return False
+    return True
+
+def part2(id_ranges):
+    invalid_ids = set()
+    for id_range in id_ranges:
+        start, end = map(int, id_range.split('-'))
+        for curr_id in range(start, end + 1):
+            for i in range(len(str(curr_id))):
+                if is_duplicate(str(curr_id), str(curr_id)[0:i+1]):
+                    invalid_ids.add(curr_id)
+                    break
+                
+                
+
+    return sum(invalid_ids)
+
+print("Part 2:", part2(id_ranges))
+
+#a = "123123123123"
+#b = "1234"
+#all_substrs = find_all(a, b)
+#print(len(all_substrs) == 0)
+#print(all_substrs[-1] + len(b) == len(a))
+
+
+#a = "824824823"
+#b = "824"
+#print(is_duplicate(a, b))
+
